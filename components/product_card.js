d68c953cc5106b19b8963bb7688774ff45e96f6b
Akinlade Feranmi <feranmiakinlade@gmail.com>
2019-12-15 13:34:29 +0000

    Going to local vscode

---
+++ components/product_card.js
@@ -0,0 +1,14 @@
+const ProductCard = (prop)=> {
+  const {}
+  <div className={`${props.className} product-card`}>
+    <span className="product-card__categories">
+      <Link href="products/[id]" as={`products/${product.id}`}>
+      <a>
+
+      </a>
+    </Link>
+    </span>
+  </div>
+};
+
+export default ProductCard;
