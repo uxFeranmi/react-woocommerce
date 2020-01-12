6a9237b1fdb3b501bdac9f5af6f01a97a772dd61
uxFeranmi <feranmiakinlade@gmail.com>
2020-01-12 21:55:33 +0100

    Saving...

--- api/modules/cart/get_cart.js
+++ api/modules/cart/get_cart.js
@@ -1,11 +1,17 @@
 const { getWooCart, fetchCartItems } = require('../../services/woo_cart');
 
+const calculateCartTotals = (cart)=> {
+
+};
+
 const getCart = async (req, res)=> {
   const cartContentIds = req.auth ?
     await getWooCart(req.auth.id)
   : req.cookie.cart;
 
   const cart = await fetchCartItems(cartContentIds);
+  cart.totals = calculateCartTotals(cart);
+
   res.status(200).json(cart);
 };
 
